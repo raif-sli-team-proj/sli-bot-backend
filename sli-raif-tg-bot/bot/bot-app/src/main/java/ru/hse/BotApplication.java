49b6da1cef2b281f8a14933c918fa88e105ae330
Lulu-fw01 <markov.lu@outlook.com>
2023-12-10 19:05:16 +0300

    new main module

--- sli-raif-tg-bot/bot/bot-app/src/main/java/ru/hse/BotApplication.java
+++ sli-raif-tg-bot/bot/bot-app/src/main/java/ru/hse/BotApplication.java
@@ -1,13 +1,13 @@
-package ru.hse.sliraiftgbot;
+package ru.hse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SliRaifTgBotApplication {
+public class BotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SliRaifTgBotApplication.class, args);
+        SpringApplication.run(BotApplication.class, args);
     }
 
 }
