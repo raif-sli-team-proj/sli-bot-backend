efa1f7dfdcd845719e4cffd1d419f638e96ca3c4
Lulu-fw01 <markov.lu@outlook.com>
2023-12-10 18:48:25 +0300

    init

---
+++ sli-raif-tg-bot/src/main/java/ru/hse/sliraiftgbot/SliRaifTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.sliraiftgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliRaifTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SliRaifTgBotApplication.class, args);
+    }
+
+}
