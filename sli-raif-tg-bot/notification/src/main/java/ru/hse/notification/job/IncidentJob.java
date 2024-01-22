128ab4101030b64b6b541fcb8b11baca8d0f865d
Lulu-fw01 <markov.lu@outlook.com>
2024-01-23 01:37:36 +0300

    job upd

--- sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/job/IncidentJob.java
+++ sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/job/IncidentJob.java
@@ -1,13 +1,14 @@
 package ru.hse.notification.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class IncidentJob implements Job {
-    public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        System.out.println("This is a quartz job!");
+    public void execute(JobExecutionContext arg0) {
+        log.info("Incident job start.");
     }
 }
