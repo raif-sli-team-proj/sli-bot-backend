b3a291eb71b5ac83b325dcb027db26bf0842ddba
Lulu-fw01 <markov.lu@outlook.com>
2024-02-10 01:34:33 +0300

    new table

---
+++ sli-raif-tg-bot/bot-app/src/main/resources/liquibase/migrations/2024-02-08-service-status-table.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset luka:quartz-migrations
+--preconditions onFail:WARN
+create table service_status
+(
+    id           SERIAL PRIMARY KEY,
+    service_name varchar(10) not null,
+    status       varchar(10) not null,
+    add_date     timestamp   not null
+)
