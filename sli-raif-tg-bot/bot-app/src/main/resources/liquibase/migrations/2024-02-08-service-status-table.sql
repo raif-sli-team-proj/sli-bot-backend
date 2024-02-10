fa5285f5c6adfeb830f982f3102628778b68d543
Lulu-fw01 <markov.lu@outlook.com>
2024-02-10 15:47:03 +0300

    table upd

--- sli-raif-tg-bot/bot-app/src/main/resources/liquibase/migrations/2024-02-08-service-status-table.sql
+++ sli-raif-tg-bot/bot-app/src/main/resources/liquibase/migrations/2024-02-08-service-status-table.sql
@@ -4,8 +4,9 @@
 --preconditions onFail:WARN
 create table service_status
 (
-    id           SERIAL PRIMARY KEY,
-    service_name varchar(10) not null,
-    status       varchar(10) not null,
-    add_date     timestamp   not null
+    id            SERIAL PRIMARY KEY,
+    service_name  varchar(10)  not null,
+    endpoint_name varchar(128) not null,
+    status        varchar(10)  not null,
+    add_date      timestamp    not null
 )
