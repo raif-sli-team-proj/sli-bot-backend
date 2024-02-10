d365747b1487a28c9ea2ab34be3b39f06685ff8c
Lulu-fw01 <markov.lu@outlook.com>
2024-02-10 15:51:46 +0300

    added new package to scan

--- sli-raif-tg-bot/bot-app/src/main/java/ru/hse/bot/BotApplication.java
+++ sli-raif-tg-bot/bot-app/src/main/java/ru/hse/bot/BotApplication.java
@@ -3,7 +3,7 @@ package ru.hse.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.hse.api", "ru.hse.bot", "ru.hse.notification"})
+@SpringBootApplication(scanBasePackages = {"ru.hse.api", "ru.hse.bot", "ru.hse.notification", "ru.hse.statistics"})
 public class BotApplication {
 
     public static void main(String[] args) {
