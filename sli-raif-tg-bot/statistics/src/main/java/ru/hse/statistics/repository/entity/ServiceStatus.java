e6b47e3438fd4aa4f0fd0a3c16bfd410ff1f4bb9
Lulu-fw01 <markov.lu@outlook.com>
2024-02-10 16:54:46 +0300

    entity upd

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/repository/entity/ServiceStatus.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/repository/entity/ServiceStatus.java
@@ -2,22 +2,28 @@ package ru.hse.statistics.repository.entity;
 
 import java.time.OffsetDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Entity
+@Table(name = "service_status")
 @Setter
 public class ServiceStatus {
-    @jakarta.persistence.Id
     @Id
     private Long id;
 
+    @Column(name = "service_name")
     private String serviceName;
+    @Column(name = "endpoint_name")
+    private String endpointName;
     @Enumerated(EnumType.STRING)
     private Status status;
+    @Column(name = "add_date")
     private OffsetDateTime addDate;
 
     public enum Status {
