b9c7b4a325462c4fca9251dd7f7fb6c19f2a4c00
Lulu-fw01 <markov.lu@outlook.com>
2024-02-11 11:26:01 +0300

    added scan

--- sli-raif-tg-bot/bot-app/src/main/java/ru/hse/bot/BotApplication.java
+++ sli-raif-tg-bot/bot-app/src/main/java/ru/hse/bot/BotApplication.java
@@ -2,13 +2,15 @@ package ru.hse.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {
         "ru.hse.api",
-        "ru.hse.bot",
         "ru.hse.statistics",
+        "ru.hse.bot",
         "ru.hse.notification"
 })
+@EntityScan("ru.hse.statistics.repository.entity")
 public class BotApplication {
 
     public static void main(String[] args) {
