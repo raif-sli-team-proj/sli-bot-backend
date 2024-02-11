822ed3056efd5f69d96327343f51c0a147ca6fbc
Lulu-fw01 <markov.lu@outlook.com>
2024-02-11 11:25:46 +0300

    entity upd

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/repository/entity/ServiceStatus.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/repository/entity/ServiceStatus.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Setter;
@@ -15,6 +17,7 @@ import lombok.Setter;
 @Setter
 public class ServiceStatus {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "service_name")
