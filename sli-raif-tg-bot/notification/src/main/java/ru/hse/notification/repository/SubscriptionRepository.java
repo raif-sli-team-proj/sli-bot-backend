e5182b0072642d5587dae604e6591c7516379a57
Lulu-fw01 <markov.lu@outlook.com>
2024-02-21 15:13:04 +0300

    subscribe flow

--- sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/repository/SubscriptionRepository.java
+++ sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/repository/SubscriptionRepository.java
@@ -1,7 +1,11 @@
 package ru.hse.notification.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hse.notification.repository.entity.Subscription;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    List<Subscription> findSubscriptionsByChatId(String chatId);
 }
