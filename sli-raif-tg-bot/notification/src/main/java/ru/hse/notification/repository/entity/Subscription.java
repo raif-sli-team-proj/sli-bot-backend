df94e8a5486178278468751fa81f9e5a24fe4b9b
Lulu-fw01 <markov.lu@outlook.com>
2024-02-21 15:12:26 +0300

    entity upd

--- sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/repository/entity/Subscription.java
+++ sli-raif-tg-bot/notification/src/main/java/ru/hse/notification/repository/entity/Subscription.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Setter;
 
 @Entity
 @Table(name = "subscription")
+@Setter
 public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
