2296fc38d0e0bcdeaee0001302fdd073307b90b1
Lulu-fw01 <markov.lu@outlook.com>
2024-02-21 16:11:01 +0300

    small fix

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/controller/SubscriptionController.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/controller/SubscriptionController.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hse.api.dto.SubscribeRequest;
 import ru.hse.notification.SubscriptionService;
 
-@RestController("api/v1/subscription")
+@RestController
+@RequestMapping("api/v1/subscription")
 @RequiredArgsConstructor
 public class SubscriptionController {
 
