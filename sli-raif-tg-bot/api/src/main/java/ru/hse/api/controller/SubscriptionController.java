126456497910b70eaa1b18495da63fc96c086746
Lulu-fw01 <markov.lu@outlook.com>
2024-03-08 19:16:54 +0300

    new endpoint

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/controller/SubscriptionController.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/controller/SubscriptionController.java
@@ -2,11 +2,14 @@ package ru.hse.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hse.api.dto.SubscribeRequest;
+import ru.hse.api.dto.SubscriptionResponse;
 import ru.hse.notification.SubscriptionService;
 
 @RestController
@@ -20,5 +23,11 @@ public class SubscriptionController {
     public ResponseEntity<?> subscribe(@RequestBody SubscribeRequest body) {
         subscriptionService.addSubscription(body.telegramChatId());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{telegramChatId}")
+    public ResponseEntity<SubscriptionResponse> isSubscribed(@PathVariable String telegramChatId) {
+        return ResponseEntity.ok(new SubscriptionResponse(telegramChatId,
+                subscriptionService.isSubscribed(telegramChatId)));
     }
 }
