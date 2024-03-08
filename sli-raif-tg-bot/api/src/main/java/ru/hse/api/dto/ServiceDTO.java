5635020719a1c596b0ae2cf698bffa4d18d8843e
Lulu-fw01 <markov.lu@outlook.com>
2024-03-08 17:52:34 +0300

    updated dto

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/ServiceDTO.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/ServiceDTO.java
@@ -10,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ServiceDTO {
+    private String serviceId;
     private String serviceName;
 
     private List<Status> statuses;
