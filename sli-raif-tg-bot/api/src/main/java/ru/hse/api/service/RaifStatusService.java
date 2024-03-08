7935a678ef4e13346172bbd54b5089fac5dca955
Lulu-fw01 <markov.lu@outlook.com>
2024-03-08 17:52:46 +0300

    updated service

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/service/RaifStatusService.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/service/RaifStatusService.java
@@ -9,15 +9,32 @@ import ru.hse.api.dto.Status;
 import java.time.LocalDate;
 import java.util.List;
 
+import ru.hse.statistics.service.StatusService;
+
 @Service
 @RequiredArgsConstructor
 public class RaifStatusService {
     private final RaifClient raifStatusClient;
+    private final StatusService statusService;
 
     public List<ServiceDTO> getFpsServicesStatuses() {
         return raifStatusClient.getFPSStatusInfo().getServices().stream()
-                .map(service -> new ServiceDTO(service.getName(), service.getStatuses(), 1.0,
-                        new Status(LocalDate.now(), "OK"))).toList();
+                .map(service -> new ServiceDTO(
+                        service.getExternalId(),
+                        service.getName(),
+                        service.getStatuses(),
+                        1.0,
+                        new Status(LocalDate.now(), countStatus(service.getExternalId()))
+                )).toList();
+    }
+
+    private String countStatus(String serviceId) {
+        // TODO wait for fix inside statusService.getServiceStatus.
+        var currentStatus = statusService.getServiceStatus(serviceId);
+        if (currentStatus == ru.hse.statistics.model.Status.OK) {
+            return "ON";
+        }
+        return "OFF";
     }
 
 }
