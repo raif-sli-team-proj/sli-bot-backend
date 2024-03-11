d2257b1373f3c7a212c1620ea5828bbe89edf71b
Федор Филиппов <fefilippov@edu.hse.ru>
2024-03-11 23:48:44 +0300

    changed comment message

--- sli-raif-tg-bot/core/src/main/java/ru/hse/core/service/CommentService.java
+++ sli-raif-tg-bot/core/src/main/java/ru/hse/core/service/CommentService.java
@@ -55,9 +55,9 @@ public class CommentService {
         Set<String> chats = subscriptionService.getAllSubscriptions().stream()
                 .map(Subscription::getChatId)
                 .collect(Collectors.toSet());
-        tgBot.sendMessages(chats, "New comment left by user" + String.format("%s: ", commentDTO.getUserId()) +
+        tgBot.sendMessages(chats, "Новый комментарий от пользователя" + String.format("%s: ", commentDTO.getUserId()) +
                 String.format("%s", commentDTO.getContents()) + '\n' +
-                String.format("Status: %s", commentDTO.getNewIncidentStatus().toString()));
+                String.format("Статус: %s", commentDTO.getNewIncidentStatus().toString()));
     }
 
     public List<CommentResponseDTO> getCommentsByIncidentId(String id) {
