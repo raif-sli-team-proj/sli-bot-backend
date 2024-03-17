b250bfc737dd6261d76b2c162acbde3d91a4d5da
Lulu-fw01 <markov.lu@outlook.com>
2024-03-17 22:44:54 +0300

    moved sli metric

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/GetSliResponse.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/GetSliResponse.java
@@ -3,6 +3,8 @@ package ru.hse.api.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import ru.hse.statistics.model.SliMetric;
+
 public record GetSliResponse(
         List<SliMetric> metrics,
         LocalDateTime leftDateTime,
