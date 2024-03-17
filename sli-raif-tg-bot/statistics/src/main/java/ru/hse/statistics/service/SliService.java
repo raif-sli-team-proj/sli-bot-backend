6b26923cdd61b7ea45fd73a918af6c69ad67fc51
Lulu-fw01 <markov.lu@outlook.com>
2024-03-18 01:57:33 +0300

    added get sli to service for hour frame

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/SliService.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/SliService.java
@@ -1,14 +1,53 @@
 package ru.hse.statistics.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.hse.statistics.model.SliMetric;
+import ru.hse.statistics.model.Status;
+import ru.hse.statistics.repository.ServiceStatusRepository;
+import ru.hse.statistics.repository.entity.ServiceStatus;
 
 @Service
+@RequiredArgsConstructor
 public class SliService {
 
-    public void getServiceSli(String serviceName, String frame, LocalDateTime leftDateTime,
-                              LocalDateTime rightDateTime) {
+    private final ServiceStatusRepository serviceStatusRepository;
 
+    public List<SliMetric> getServiceSliForMonth(String serviceName, String frame) {
+        OffsetDateTime thirtyDaysAgo = OffsetDateTime.now(ZoneOffset.UTC).minusDays(30);
+
+        List<ServiceStatus> statuses = serviceStatusRepository.findAllStatusesAfter(serviceName,
+                OffsetDateTime.from(thirtyDaysAgo));
+
+        return switch (frame) {
+            case "1h" -> getSliForHour(statuses);
+            case "1d" -> Collections.emptyList();
+            case "1w" -> Collections.emptyList();
+            default -> Collections.emptyList();
+        };
+    }
+
+    private List<SliMetric> getSliForHour(List<ServiceStatus> statuses) {
+       var result = statuses.stream()
+                .collect(Collectors.groupingBy(status ->
+                                status.getAddDate().withMinute(0).withSecond(0).withNano(0),
+                        Collectors.collectingAndThen(Collectors.toList(), statusesByHour -> {
+                            double failedCount =
+                                    statusesByHour.stream().filter(s -> s.getStatus() == Status.FAIL).toList().size();
+                            double totalCount = statusesByHour.size();
+                            return 1 - failedCount / totalCount;
+                        })));
+
+        return result.entrySet().stream()
+                .map(e -> new SliMetric(e.getValue(), e.getKey().toLocalDateTime()))
+                .sorted(Comparator.comparing(SliMetric::dateTime))
+                .toList();
     }
 }
