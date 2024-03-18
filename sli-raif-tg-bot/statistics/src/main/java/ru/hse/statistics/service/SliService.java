0e8e040406772f4d06f8a769e5ca57d5fa0bae10
Lulu-fw01 <markov.lu@outlook.com>
2024-03-18 14:01:34 +0300

    added logic for week frame

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/SliService.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/SliService.java
@@ -1,10 +1,13 @@
 package ru.hse.statistics.service;
 
+import java.time.DayOfWeek;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
@@ -27,25 +30,51 @@ public class SliService {
                 OffsetDateTime.from(thirtyDaysAgo));
 
         return switch (frame) {
-            case "1h" -> getSliForHour(statuses);
-            case "1d" -> Collections.emptyList();
-            case "1w" -> Collections.emptyList();
+            case "1h" -> getSliForHours(statuses);
+            case "1d" -> getSliForDays(statuses);
+            case "1w" -> getSliForWeeks(statuses);
             default -> Collections.emptyList();
         };
     }
 
-    private List<SliMetric> getSliForHour(List<ServiceStatus> statuses) {
+    private List<SliMetric> getSliForHours(List<ServiceStatus> statuses) {
        var result = statuses.stream()
                 .collect(Collectors.groupingBy(status ->
                                 status.getAddDate().withMinute(0).withSecond(0).withNano(0),
-                        Collectors.collectingAndThen(Collectors.toList(), statusesByHour -> {
-                            double failedCount =
-                                    statusesByHour.stream().filter(s -> s.getStatus() == Status.FAIL).toList().size();
-                            double totalCount = statusesByHour.size();
-                            return 1 - failedCount / totalCount;
-                        })));
+                        Collectors.collectingAndThen(Collectors.toList(), this::countSli)));
 
-        return result.entrySet().stream()
+        return getSliMetrics(result);
+    }
+
+    private List<SliMetric> getSliForDays(List<ServiceStatus> statuses) {
+        var result = statuses.stream()
+                .collect(Collectors.groupingBy(status ->
+                                status.getAddDate().withHour(0).withMinute(0).withSecond(0).withNano(0),
+                        Collectors.collectingAndThen(Collectors.toList(), this::countSli)));
+
+        return getSliMetrics(result);
+    }
+
+    private List<SliMetric> getSliForWeeks(List<ServiceStatus> statuses) {
+        var result = statuses.stream()
+                .collect(Collectors.groupingBy(status ->
+                                status.getAddDate()
+                                        .withHour(0).withMinute(0).withSecond(0).withNano(0)
+                                        .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)),
+                        Collectors.collectingAndThen(Collectors.toList(), this::countSli)));
+
+        return getSliMetrics(result);
+    }
+
+    private Double countSli(List<ServiceStatus> statuses) {
+        double failedCount =
+                statuses.stream().filter(s -> s.getStatus() == Status.FAIL).toList().size();
+        double totalCount = statuses.size();
+        return 1 - failedCount / totalCount;
+    }
+
+    private List<SliMetric> getSliMetrics(Map<OffsetDateTime, Double> dateTimeToSli) {
+        return dateTimeToSli.entrySet().stream()
                 .map(e -> new SliMetric(e.getValue(), e.getKey().toLocalDateTime()))
                 .sorted(Comparator.comparing(SliMetric::dateTime))
                 .toList();
