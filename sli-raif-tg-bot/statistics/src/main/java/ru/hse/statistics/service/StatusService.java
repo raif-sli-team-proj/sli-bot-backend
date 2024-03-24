2348baaca7f54b7c8a5849b077e59084ab060afd
Lulu-fw01 <markov.lu@outlook.com>
2024-03-25 01:04:41 +0300

    new method

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/StatusService.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/StatusService.java
@@ -2,9 +2,15 @@ package ru.hse.statistics.service;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.hse.statistics.client.OnlineServiceStatusClient;
+import ru.hse.statistics.enumeration.RaifProduct;
+import ru.hse.statistics.enumeration.RaifService;
 import ru.hse.statistics.model.Status;
 import ru.hse.statistics.repository.ServiceStatusRepository;
 import ru.hse.statistics.repository.entity.ServiceStatus;
@@ -13,6 +19,7 @@ import ru.hse.statistics.repository.entity.ServiceStatus;
 @RequiredArgsConstructor
 public class StatusService {
     private final ServiceStatusRepository serviceStatusRepository;
+    private final OnlineServiceStatusClient serviceStatusClient;
 
     public Status getServiceStatus(String serviceName) {
         int randomNum = (int) (Math.random() * 10);
@@ -30,5 +37,18 @@ public class StatusService {
         e.setAddDate(OffsetDateTime.now(ZoneOffset.UTC));
 
         serviceStatusRepository.save(e);
+    }
+
+    public Map<RaifService, Status> getProductServiceStatuses(RaifProduct product) {
+        var response = serviceStatusClient.getOnlineStatusesByProduct(product);
+        var services = response.services();
+        return services.stream().collect(Collectors.toMap(
+                s -> RaifService.of(product.productName, s.externalId()),
+                s -> switch (s.online()) {
+                    case "ON" -> Status.OK;
+                    case "OFF" -> Status.FAIL;
+                    default -> Status.UNKNOWN;
+                }
+        ));
     }
 }
