913af3a7397735628c8e03fb9fe7acc2fb6ed39a
Lulu-fw01 <markov.lu@outlook.com>
2024-03-25 01:48:56 +0300

    new method

--- sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/StatusService.java
+++ sli-raif-tg-bot/statistics/src/main/java/ru/hse/statistics/service/StatusService.java
@@ -2,20 +2,24 @@ package ru.hse.statistics.service;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.hse.statistics.client.OnlineServiceStatusClient;
 import ru.hse.statistics.enumeration.RaifProduct;
 import ru.hse.statistics.enumeration.RaifService;
+import ru.hse.statistics.model.ProductOnlineStatusesResponse;
 import ru.hse.statistics.model.Status;
 import ru.hse.statistics.repository.ServiceStatusRepository;
 import ru.hse.statistics.repository.entity.ServiceStatus;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class StatusService {
     private final ServiceStatusRepository serviceStatusRepository;
@@ -31,16 +35,50 @@ public class StatusService {
 
     public void addStatus(Status status) {
         var e = new ServiceStatus();
-        e.setServiceName("FPS");
-        e.setEndpointName("QRC");
+        e.setProductName("FPS");
+        e.setServiceName("QRC");
         e.setStatus(status);
         e.setAddDate(OffsetDateTime.now(ZoneOffset.UTC));
 
         serviceStatusRepository.save(e);
     }
 
+    @Transactional
+    public void addStatus(RaifService service, Status status) {
+        serviceStatusRepository.save(
+                ServiceStatus.builder()
+                        .productName(service.product)
+                        .serviceName(service.service)
+                        .status(status)
+                        .addDate(OffsetDateTime.now(ZoneOffset.UTC))
+                        .build());
+    }
+
+    @Transactional
+    public void addStatuses(Map<RaifService, Status> serviceToStatus) {
+        OffsetDateTime dateTime = OffsetDateTime.now(ZoneOffset.UTC);
+        List<ServiceStatus> statuses = serviceToStatus.entrySet().stream()
+                .map(e -> ServiceStatus.builder()
+                        .productName(e.getKey().product)
+                        .serviceName(e.getKey().service)
+                        .status(e.getValue())
+                        .addDate(dateTime)
+                        .build()
+                ).toList();
+        serviceStatusRepository.saveAll(statuses);
+    }
+
+    /**
+     * Получить статусы сервисов по продукту.
+     */
     public Map<RaifService, Status> getProductServiceStatuses(RaifProduct product) {
-        var response = serviceStatusClient.getOnlineStatusesByProduct(product);
+        ProductOnlineStatusesResponse response;
+        try {
+            response = serviceStatusClient.getOnlineStatusesByProduct(product);
+        } catch (Exception e) {
+            log.error(String.format("Error getting statuses for product %s", product.productName), e);
+            return Map.of();
+        }
         var services = response.services();
         return services.stream().collect(Collectors.toMap(
                 s -> RaifService.of(product.productName, s.externalId()),
