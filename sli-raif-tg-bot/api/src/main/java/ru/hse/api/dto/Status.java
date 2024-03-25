2726c9d09efa60d031d0fd7ee643d02125d362b3
Lulu-fw01 <markov.lu@outlook.com>
2024-03-26 01:33:10 +0300

    controller fix

--- sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/Status.java
+++ sli-raif-tg-bot/api/src/main/java/ru/hse/api/dto/Status.java
@@ -1,16 +1,9 @@
 package ru.hse.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Status {
-    private LocalDate eventDate;
 
-    private String status;
+public record Status(LocalDate eventDate, String status) {
 }
+
