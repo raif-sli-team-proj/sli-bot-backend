553df0b58fe8b82170ac6b0600c548847025a85c
Федор Филиппов <fefilippov@edu.hse.ru>
2024-03-26 17:51:09 +0300

    admin test

--- sli-raif-tg-bot/core/src/test/java/ru/hse/core/service/AdminServiceTest.java
+++ sli-raif-tg-bot/core/src/test/java/ru/hse/core/service/AdminServiceTest.java
@@ -1,7 +1,31 @@
 package ru.hse.core.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.hse.core.repository.AdminRepository;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AdminServiceTest {
+    @Mock
+    private AdminRepository adminRepository;
+
+    @InjectMocks
+    private AdminService adminService;
+
+    @Test
+    public void whenUserIsAdmin_thenReturnsTrue() {
+        String adminUserId = "adminUser";
+        when(adminRepository.existsById(adminUserId)).thenReturn(true);
+
+        Boolean isAdmin = adminService.checkIfUserIsAdmin(adminUserId);
+
+        assertTrue(isAdmin);
+    }
 
 }
